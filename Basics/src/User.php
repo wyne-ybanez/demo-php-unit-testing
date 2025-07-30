728fe8a867394b73f0559cb991b869642c4f816c
wyne-ybanez <wyne_01@icloud.com>
2025-07-30 16:51:39 +0100

    update: adds user class and test for public methods

---
+++ Basics/src/User.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+class User
+{
+    private string $username;
+    private string $password_hash;
+
+    public function __construct(string $username, string $password)
+    {
+        $this->username = $username;
+        $this->password_hash = $this->hashPassword($password);
+    }
+
+    public function authenticatePassword(string $password): bool
+    {
+        // Additional user checks, e.g. is active
+        // ...
+
+        return $this->verifyPassword($password);
+    }
+
+    protected function hashPassword(string $password): string
+    {
+        // you would generally use `password_hash()` in a real world setting - but for this example we will just use `hash`
+        return hash('sha256', $password);
+    }
+
+    private function verifyPassword(string $password): bool
+    {
+        return $this->password_hash === $this->hashPassword($password);
+    }
+}
---
+++ Basics/tests/UserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    public function testCorrectPasswordAuthenticatesSuccessfully(): void
+    {
+        $user = new User('Dave', 'sample_password');
+
+        $this->assertTrue($user->authenticatePassword('sample_password'));
+    }
+
+    public function testIncorrectPasswordFailsAuthentication(): void
+    {
+        $user = new User('Cian', 'cian_secret_password');
+
+        $this->assertFalse($user->authenticatePassword('asdasdasda'));
+    }
+
+    public function testPasswordHashIsMinimumLength(): void
+    {
+        $user = new User('Ben', 'ben_secret_password');
+
+        $hash = $user->hashPassword('ben_secret_password');
+
+        $this->greaterThanOrEqual(60, strlen($hash));
+    }
+}
